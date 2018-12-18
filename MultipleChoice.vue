ee5fe785eec77eaeb503e2567be01569e0b1c272
ZsandaX <zsanda520@gmail.com>
2018-12-18 21:24:52 +0800

    +hello world

---
+++ MultipleChoice.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="custom-control custom-radio" v-for="(text, key) of description" :key="key">
+      <input type="radio" :name="name" :value="key" class="custom-control-input" :id="name+key" @input="$emit('update', $event.target.value*1)">
+      <label class="custom-control-label" :for="name+key" v-if="key>0">
+        ({{key}}) {{text}}
+      </label>
+      <div v-if="key==0" style="margin-left: -1.5rem;margin-bottom: 1rem;">{{text}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  props: ["description"],
+  data: function() {
+    return {
+      name: Math.random().toString(36).substring(2)
+    };
+  },
+  created() {
+    this.$emit("update", 0);
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ TrueOrFalse.vue
@@ -0,0 +1,30 @@
+<template>
+  <label class="question">
+    <select @change="$emit('update', $event.target.value*1)" class="rounded font-weight-bold p-1">
+      <option v-for="(option, key) of options" :value="key" :key="key">{{option}}</option>
+    </select>
+    <slot></slot>
+    <span class="pl-1">{{description}}</span>
+  </label>
+</template>
+
+<script>
+module.exports = {
+  props: ["description"],
+  data: function() {
+    return {
+      options: ["請選擇", "◯", "╳"]
+    };
+  },
+  created() {
+    this.$emit("update", 0);
+  }
+};
+</script>
+
+<style scoped>
+.rounded {
+  text-align: center;
+  text-align-last: center;
+}
+</style>
